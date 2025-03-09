e69c2027be9a55a1ac699f24be61ecb5f3b7d899
Muhammad Bilal <sheikhbilaal085@gmail.com>
2025-03-09 23:59:10 +0500

    My result

---
+++ project_result.py
@@ -0,0 +1,20 @@
+Name = str("Name: Muhammad Bilaal")
+print(Name)
+Roll_no = ("Roll_no: 45745")
+print(Roll_no)
+Program = str("Program: PIAIC")
+print(Program)
+Batch = ("Batch: 67")
+print(Batch)
+Quarter = ("Quarter: 2")
+print(Quarter)
+result = True
+if result:
+    print(f"""Congratulations🎉, You have passed all your Python Projects! 🐍🚀
+Dear Muhammad Bilaal,
+Great job on successfully completing and passing your Python project! 🏆 Your dedication, problem-solving skills, and coding expertise have truly paid off.This achievement is a testament to your hard work and passion for programming.💻✨
+Keep exploring, keep coding, and aim for even greater success in the future! 🚀🔥
+Wishing you all the best in your programming journey! 🎯👏""")
+    print("See you soon in next quarter......!😇")
+else:
+    print("Better luck for next time😢")
